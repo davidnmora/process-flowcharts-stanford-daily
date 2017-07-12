2b87b9d5838621df144ea2c6bddcb45377ce719f
davidnmora <dmora1@stanford.edu>
2017-07-11 20:02:06 -0700

    Do bulk of title-ix styling

--- js/title-ix.js
+++ js/title-ix.js
@@ -1,7 +1,13 @@
+var colors = {
+	textColor: "white",
+	complainantTextColor: "#7FBC78",
+	accusedTextColor: "#E56A42"
+}
+
 var nodeBodyTextById = {  
 	a:  "",
 	b:  "",
-	c:  "Title IX: A. recognises a party involved in incident report as repeat offender and/or B. deems the situation so severe it demands further investigation despite complainant declining it.",
+	c:  "In rare cases, Title IX may: A. recognise a party involved in the incident report as repeat offender and/or B. deem the situation so severe it demands further investigation despite complainant declining it.",
 	d:  "",
 	e:  "Investigation begins. Parties sent a Notice of Concern, and interim measures are implemented on a case-by-case basis. Title IX investigator investigates by interviewing parties and witnesses and by collecting relevant documentary evidence. Title IX Coordinator makes a charging decision and notifies students",
 	f1: "Outcome Letter Issued. Dissatisfied parties may appeal.",
@@ -10,8 +16,8 @@ var nodeBodyTextById = {
 	g:  "Evidentiary Specialist (outside of Stanford) reviews any objections relating to contents of Hearing File. Parties submit statements in response to the Hearing File 1. Three trained panelists review Hearing File, meet with parties and witnesses 2. Deliberate and determine responsibility using preponderance of the evidence standard 3. Parties notified of outcome",
 	h:  "",
 	i:  "Sanctioning and Remedies imposed. 1. Both parties may submit sanction statement to the panelists 2. Hearing panel determines disciplinary outcome and Title IX Coordinator sets ongoing remedies for protection of complainant and campus 3. Outcome Letter issued to parties",
-	j:  "Either party may appeal Outcome Letter after hearing or no charge. Automatic appeal to Provost if expulsion upheld",
-	
+	j:  "Either party may appeal Outcome Letter after hearing or no charge. Automatic appeal to Provost if expulsion upheld.",
+	// k currently unused
 	l:  "",
 	m:  ""
 }
@@ -58,7 +64,6 @@ function instantiateTooltips() {
 		var nodeDiv = $("#" + id + " div");
 		nodeDiv.prop("title", nodeBodyTextById[id]);
 		if (nodeBodyTextById[id] !== "") { 
-			console.log("underlined:", id);
 			nodeDiv.css("text-decoration", "underline"); // indicite there's more info on hover
 		}
 	});
@@ -69,11 +74,11 @@ function instantiateTooltips() {
 
 
 function highlightInvolvements() {
-	onNodeMouseEnter("show-complainant", complainantNodeIds, "red");
-	onNodeMouseLeave("show-complainant", complainantNodeIds, "black");
+	onNodeMouseEnter("show-complainant", complainantNodeIds, colors.complainantTextColor);
+	onNodeMouseLeave("show-complainant", complainantNodeIds, colors.textColor);
 
-	onNodeMouseEnter("show-accused", accusedNodeIds, "red");
-	onNodeMouseLeave("show-accused", accusedNodeIds, "black");
+	onNodeMouseEnter("show-accused", accusedNodeIds, colors.accusedTextColor);
+	onNodeMouseLeave("show-accused", accusedNodeIds, colors.textColor);
 }
 
 
